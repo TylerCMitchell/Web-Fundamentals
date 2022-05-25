d3eee4e9a72d3aad56dcf3d4b269f9e72d09b7cf
Tyler Mitchell <mitchellctyler@gmail.com>
2022-05-24 19:02:16 -0700

    initial commit

---
+++ Likes/Part 1/script.js
@@ -0,0 +1,3 @@
+function addlike() {
+  document.querySelector("#likegrow").innerText++;
+}
---
+++ Likes/Part 2/script.js
@@ -0,0 +1,9 @@
+function addlike() {
+  document.querySelector("#likegrow").innerText++;
+}
+function addhike() {
+  document.querySelector("#likebrow").innerText++;
+}
+function addbike() {
+  document.querySelector("#likecrow").innerText++;
+}
